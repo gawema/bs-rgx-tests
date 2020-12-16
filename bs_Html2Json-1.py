6351866469717651549934f3a77f8b2234cd3de2
gawema <gab.mannu@hotmail.it>
2020-12-16 23:49:24 +0100

    new version

--- bs_Html2Json-1.py
+++ bs_Html2Json-1.py
@@ -2,34 +2,31 @@ import pprint
 import htmlmin
 from bs4 import BeautifulSoup, ResultSet, element as Element
 
-def convetToJson(el=None, json={}):
+def convetToJson(el=None, newjson={}):
 	if type(el) == Element.Tag:
-		json[el.name] = {'attributes': el.attrs}
-		json[el.name] = convetToJson(el.contents,json[el.name])
-		return json[el.name]
-	elif len(el):
-		elements = []
-		text = ''
-		for el in el:
-			if type(el) == Element.Tag:
-				value = convetToJson(el,json)
-				elements.append({el.name:value})
+		print('tag')
+		newjson = {
+			'name': el.name,
+			'attributes': el.attrs,
+			'child_components': []
+		}
+		for child in el.contents:
+			if type(child) == Element.Tag:
+				childComponent = convetToJson(child, newjson)
+				newjson['child_components'].append(childComponent)
 			else:
-				text += el.strip()
-		json['text'] = text
-		return json
-	else:
-		return json
+				newjson['text'] = child
+		return newjson
 
 with open("index.html",'r') as file:
 	html = file.read()
 	minified_html=htmlmin.minify(html)
 	soup = BeautifulSoup(minified_html, "html.parser")
-	components = soup.findAll("editable-paragraph")
+	components = soup.findAll("editable")
 	jComponents = []
 	jComponent = {}
 	for component in components:
-		jComponent[component.name] = convetToJson(component)
+		jComponent = convetToJson(component)
 		jComponents.append(jComponent.copy())
 	
 	pprint.pprint(jComponents)
